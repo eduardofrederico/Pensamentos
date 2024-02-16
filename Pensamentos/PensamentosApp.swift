ca887ff1a3e6ab0910500607eaf36a1caf1ebedc
Eduardo Frederico <eduardo.frederico@icloud.com>
2024-02-15 22:52:48 -0300

    Initial Commit

---
+++ Pensamentos/PensamentosApp.swift
@@ -0,0 +1,17 @@
+//
+//  PensamentosApp.swift
+//  Pensamentos
+//
+//  Created by Eduardo Frederico on 15/02/24.
+//
+
+import SwiftUI
+
+@main
+struct PensamentosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
